faac058744a0217fb15c7a06d232bded5da20d47
TahirRehman1995 <49286139+TahirRehman1995@users.noreply.github.com>
2023-04-01 03:36:44 +0500

    Add files via upload

---
+++ Bano Qabil Assignment 3/JavaScript/index.js
@@ -0,0 +1,24 @@
+
+/*------The Que 1 in assignment 3-----*/
+
+let age=25;
+let AgeAsString =String(age);
+console.log("The Data you typed is converted to string data type",typeof(AgeAsString));
+
+
+/*------The Que 3 in Assignment 3-----*/
+let Temprature =72;
+let celceius =(Temprature-32) *5/9;
+console.log(celceius)
+
+/*------The Que 5 in assignment 3-----*/
+
+let isRaining =true;
+if (isRaining) {
+    console.log("Please, Bring an Umbrella")
+} else {
+    console.log("Please, Leave the Umbrella at Home")
+}
+
+
+
